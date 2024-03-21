d65af485c0c916369edf1a295b6e2f71ef61dd15
Jonas <159673319+JonsCampos@users.noreply.github.com>
2024-03-21 16:45:46 -0300

    Add files via upload

--- esboço/main.py
+++ esboço/main.py
@@ -1,6 +1,8 @@
 import sqlite3 as conector
 
-def ins_ent(conexao, cursor):
+def ins_ent():
+    conexao = conector.connect("./banco_rad.db") 
+    cursor = conexao.cursor()
     id_func = input('Digite o id do funcionário: ')
     id_func = id_func.strip()
     id_local_ent = input('Digite o id do local: ')
@@ -15,21 +17,31 @@ def ins_ent(conexao, cursor):
     else:
         conexao.rollback()
         print('Entrada cancelada!')
+    cursor.close()
+    conexao.close()
 
-def list_ent(cursor):
+
+def list_ent():
+    conexao = conector.connect("./banco_rad.db") 
+    cursor = conexao.cursor()
     comando = '''SELECT f.nome, s.nome_setor, l.nome_local,  strftime('%d/%m/%Y %H:%M', e.data_hora_ent)
-                FROM Entrada e
-                JOIN Funcionario f ON e.funcionario_ID = f.funcionario_ID
-                JOIN Setor s ON f.setor_ID = s.setor_ID
-                JOIN Local l ON e.local_ID = l.local_ID
-                ORDER BY e.data_hora_ent DESC;'''
+                    FROM Entrada e
+                    JOIN Funcionario f ON e.funcionario_ID = f.funcionario_ID
+                    JOIN Setor s ON f.setor_ID = s.setor_ID
+                    JOIN Local l ON e.local_ID = l.local_ID
+                    ORDER BY e.data_hora_ent DESC;'''
     cursor.execute(comando)
     registros = cursor.fetchall()
     print('\nLista de Entradas')
     for registro in registros:
         print(registro)
+    cursor.close()
+    conexao.close()
 
-def ins_sai(conexao, cursor):
+
+def ins_sai():
+    conexao = conector.connect("./banco_rad.db") 
+    cursor = conexao.cursor()
     id_func = input('Digite o id do funcionário: ')
     id_func = id_func.strip()
     id_local_sai = input('Digite o id do local: ')
@@ -44,33 +56,34 @@ def ins_sai(conexao, cursor):
     else:
         conexao.rollback()
         print('Saída cancelada!')
+    cursor.close()
+    conexao.close()
 
-def list_sai(cursor):
+
+def list_sai():
+    conexao = conector.connect("./banco_rad.db") 
+    cursor = conexao.cursor()
     comando = '''SELECT f.nome, st.nome_setor, l.nome_local, strftime('%d/%m/%Y %H:%M', sd.data_hora_sai) 
-                FROM Saida sd
-                JOIN Funcionario f ON sd.funcionario_ID = f.funcionario_ID
-                JOIN Setor st ON f.setor_ID = st.setor_ID
-                JOIN Local l ON sd.local_ID = l.local_ID
-                ORDER BY sd.data_hora_sai DESC;'''
+                    FROM Saida sd
+                    JOIN Funcionario f ON sd.funcionario_ID = f.funcionario_ID
+                    JOIN Setor st ON f.setor_ID = st.setor_ID
+                    JOIN Local l ON sd.local_ID = l.local_ID
+                    ORDER BY sd.data_hora_sai DESC;'''
     cursor.execute(comando)
     registros = cursor.fetchall()
     print('\nLista de Saídas')
     for registro in registros:
         print(registro)
+    cursor.close()
+    conexao.close()
 
-
-conexao = conector.connect("./banco_rad.db") 
-cursor = conexao.cursor()
 
 opc = int(input('1 - Inserir Entrada | 2 - Listar Entradas | 3 - Inserir Saída | 4 - Listar Saídas\n'))
 if opc == 1:
-    ins_ent(conexao, cursor)
+    ins_ent()
 elif opc == 2:
-    list_ent(cursor)
+    list_ent()
 elif opc == 3:
-    ins_sai(conexao, cursor)
+    ins_sai()
 elif opc == 4:
-    list_sai(cursor)
-
-cursor.close()
-conexao.close()
+    list_sai()
